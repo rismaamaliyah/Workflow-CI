8dee42dffac8952449837fc7bd9795ecf262138c
rismaamaliyah <rismaamaliyah2@gmail.com>
2026-07-04 09:30:03 +0700

    updated

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -8,13 +8,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--max_iter", type=int, default=100)
 args = parser.parse_args()
 
-mlflow.autolog(log_models=True)
-
 train_df = pd.read_csv("adult_income_dataset_preprocessing.csv")
 X_train = train_df.drop(columns=["income"])
 y_train = train_df["income"]
 
-model = LogisticRegression(max_iter=args.max_iter)
-model.fit(X_train, y_train)
-acc = accuracy_score(y_train, model.predict(X_train))
-print(f"Training accuracy: {acc}")
+with mlflow.start_run():
+    model = LogisticRegression(max_iter=args.max_iter)
+    model.fit(X_train, y_train)
+    acc = accuracy_score(y_train, model.predict(X_train))
+    mlflow.log_param("max_iter", args.max_iter)
+    mlflow.log_metric("accuracy", acc)
+    mlflow.sklearn.log_model(model, "model")
+    print(f"Training accuracy: {acc}")
