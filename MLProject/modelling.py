1b2a942e0467b2f65e07c405ff7996c50cbaeae9
rismaamaliyah <rismaamaliyah2@gmail.com>
2026-07-04 08:32:59 +0700

    Edited modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,21 +2,20 @@ import mlflow
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-import joblib
+import argparse
 
-# load dataset
-train_df = pd.read_csv("MLProject/adult_income_dataset_preprocessing.csv")
+parser = argparse.ArgumentParser()
+parser.add_argument("--max_iter", type=int, default=100)
+args = parser.parse_args()
+
+mlflow.autolog(log_models=True)
+
+train_df = pd.read_csv("adult_income_dataset_preprocessing.csv")
 X_train = train_df.drop(columns=["income"])
 y_train = train_df["income"]
 
-# train model
-model = LogisticRegression(max_iter=1000)
-model.fit(X_train, y_train)
-
-# log metric
-acc = accuracy_score(y_train, model.predict(X_train))
-mlflow.log_metric("accuracy", acc)
-
-# save model
-joblib.dump(model, "adult_income_model.pkl")
-mlflow.log_artifact("adult_income_model.pkl", artifact_path="models")
+with mlflow.start_run():
+    model = LogisticRegression(max_iter=args.max_iter)
+    model.fit(X_train, y_train)
+    acc = accuracy_score(y_train, model.predict(X_train))
+    print(f"Training accuracy: {acc}")
